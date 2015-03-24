74c1d903f5187cdb6146bef2763915fe7af29b9c
Nattawit Laorit <msn_coach@hotmail.com>
2015-03-24 16:47:40 +0700

    Import CSV Functuion

--- src/com/example/karfai/MainActivity.java
+++ src/com/example/karfai/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.karfai;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import com.opencsv.CSVReader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +21,35 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        URL url = getClass().getResource("test.csv");
+        //File file = new File(url.getPath());
+        String csvFilename = "..//test.csv";
+        CSVReader csvReader = null;
+		try {
+			csvReader = new CSVReader(new InputStreamReader(getAssets().open("test.csv")));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		};
+        String[] row = null;
+        try {
+			while((row = csvReader.readNext()) != null) {
+			    System.out.println(row[0]
+			              + " # " + row[1]
+			              + " #  " + row[2]);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			csvReader.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 
@@ -33,4 +71,6 @@ public class MainActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
+    
 }
